05469125b771431e1df00646cccd84dae24c56bf
paundo <paundo@gmail.com>
2020-07-12 22:06:41 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,58 @@
+const http = require('http');
+
+const https = require('https');
+const port = process.env.PORT || 8080;
+const BotEvents = require('viber-bot').Events;
+
+const ViberBot = require('viber-bot').Bot;
+const TextMessage = require('viber-bot').Message.Text;
+
+const KeyboardMessage = require('viber-bot').Message.Keyboard;
+const PictureMessage = require('viber-bot').Message.Picture;
+const UrlMessage = require('viber-bot').Message.Url;
+const request = require('request');
+
+
+const nastaviKeyboard = require('./keyboards/nastaviKeyboard.json');
+const pravopisGramatikaPocetnaKeyboard = require('./keyboards/pravopisGramatikaPocetnaKeyboard.json');
+
+
+
+
+function say(response, message) {
+  response.send(new TextMessage(message,bttnPocetnaKeyboard));
+}
+
+const bot = new ViberBot({
+  authToken: process.env.VIBER_PUBLIC_ACCOUNT_ACCESS_TOKEN_KEY,
+  name: "PetarChatBot",
+  avatar: "https://raw.githubusercontent.com/paundo/viber-bot/master/pravopis.png" // It is recommended to be 720x720, and no more than 100kb.
+});
+
+//bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFinish) => response.send(new KeyboardMessage(pocetnaKeyboard)) );
+
+bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) =>{
+  onFinish(new TextMessage(`Здраво! Ја сам PetarChatBot!`,nastaviKeyboard));
+});
+
+/*
+bot.onSubscribe(response => {
+  say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you. Just send me a name of a website and I'll do the rest!`);
+});
+*/
+bot.onTextMessage(/pravopis-gramatika/i, (message, response) =>
+  response.send(new KeyboardMessage(pravopisGramatikaPocetnaKeyboard)));
+
+
+
+bot.onTextMessage(/./i, (message, response) => 
+  response.send(new TextMessage("Појам који сте тражили није тренутно евидентиран у бази знања.",bttnPocetnaKeyboard));
+
+
+const webhookUrl = process.env.WEBHOOK_URL;
+
+const server = http.createServer(bot.middleware()).listen(port, () => bot.setWebhook(process.env.WEBHOOK_URL));
+
+
+
+}
